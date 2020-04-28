fe314aada8886ed993b5ca638fda5b36ad5e18f0
bumchik2 <elisey_sudakov@mail.ru>
2020-04-28 21:55:49 +0300

    Fixed missprint

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-from telegram_bot import BotManager
+from telegram_bot import BotManager, debug_function
 from tests import test_all
 from flask import Flask, request
 import telebot
 import config
 import os
 
-
 server = Flask(__name__)
 
 
 @server.route('/' + config.TOKEN, methods=['POST'])
+@debug_function
 def get_message():
     BotManager.bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
     return '!', 200
 
 
 @server.route('/')
+@debug_function
 def web_hook():
     BotManager.bot.remove_webhook()
-    url = 'https://peaceful-shelf-93340.herokuapp.com/' + config.TOKENp
+    url = 'https://peaceful-shelf-93340.herokuapp.com/' + config.TOKEN
     BotManager.bot.set_webhook(url=url)
     return '!', 200
 
 
+@debug_function
 def run_bot():
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
+@debug_function
 def main():
     test_all()
     run_bot()
