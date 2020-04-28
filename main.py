baee2c0a16a7a9ff657b62250be9bf5470efc8f5
bumchik2 <elisey_sudakov@mail.ru>
2020-04-28 21:21:48 +0300

    Fixed a small misprint mistake

--- main.py
+++ main.py
@@ -6,22 +6,25 @@ import config
 import os
 
 
+server = Flask(__name__)
+
+
+@server.route('/' + config.TOKEN, methods=['POST'])
+def get_message():
+    BotManager.bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
+    return '!', 200
+
+
+@server.route('/')
+def web_hook():
+    BotManager.bot.remove_webhook()
+    url = 'https://mytalkingbot.herokuapp.com/' + config.TOKENp
+    BotManager.bot.set_webhook(url=url)
+    return '!', 200
+
+
 def run_bot():
-    server = Flask(__name__)
-
-    @server.route("/" + config.TOKEN, methods=['POST'])
-    def get_message():
-        BotManager.bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-
-    @server.route("/")
-    def web_hook():
-        BotManager.bot.remove_webhook()
-        url = 'https://mytalkingbot.herokuapp.com/' + config.TOKEN
-        BotManager.bot.set_webhook(url=url)
-        return "!", 200
-
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 def main():
